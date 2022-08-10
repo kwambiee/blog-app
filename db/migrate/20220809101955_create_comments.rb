fc674ff53258778abea49d3d1cc98396fa1a99d0
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-10 12:19:45 +0300

    Add comment counter method

--- db/migrate/20220809101955_create_comments.rb
+++ db/migrate/20220809101955_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
       # t.belongs_to :user, index: true, foreign_key: true
       # t.belongs_to :post, index: true, foreign_key: true
-      self.primary_key = 'id'
       t.references :author, null:false, foreign_key: { to_table: 'users' }
       t.references :post, null:false, foreign_key: { to_table: 'posts' }
       t.string :text
