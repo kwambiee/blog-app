860e9b04c255f3cf5aed9775feaa8f9fde15716d
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-11 03:16:12 +0300

    Add validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name,presence:true
+
   has_many :posts, foreign_key: 'author_id', class_name: 'Post'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
   has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
