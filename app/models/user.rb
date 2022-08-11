9721f336cd0083190f4788f8b932494f606d99e6
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-11 04:38:58 +0300

    Add test for comment and likes model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name,presence:true
-  validates :post_counter, numeracality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :post_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
   has_many :posts, foreign_key: 'author_id', class_name: 'Post'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
