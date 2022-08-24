7aad907b20c874b7fb40ff78eae04c1471c440bd
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-24 08:11:23 +0300

    Add authenticate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  # validates :name, presence: true
-  # validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 
   has_many :posts, foreign_key: 'author_id', class_name: 'Post'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
