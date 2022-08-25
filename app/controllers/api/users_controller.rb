0fad2c021f57c7c8ae41e9e05cfc182dc35ccec6
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-25 10:18:00 +0300

    Render users as json

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::UsersController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        @users = User.all
+        respond_to do |format|
+            format.json { render json: @users }
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        respond_to do |format|
+            format.json { render json: @user }
+        end
+    end
+    end
