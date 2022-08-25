d7b943ff1d390ecca495c983aee5f11e7a037fef
[Dinma-Faith] <[phaytep@gmail.com]>
2022-08-25 11:52:54 +0100

    Fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,35 +1,35 @@
 class Api::V1::CommentsController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    def index
-        @user=User.find(params[:user_id])
-        @posts = @user.posts.find(params[:post_id])
-        @comments = @posts.comments
-        respond_to do |format|
-            format.json { render json: @comments }
-        end
+  skip_before_action :verify_authenticity_token
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.find(params[:post_id])
+    @comments = @posts.comments
+    respond_to do |format|
+      format.json { render json: @comments }
     end
+  end
 
-    def create
-        @user=User.find(params[:user_id])
-        @post = @user.posts.find(params[:post_id])
-        @comment = @post.comments.create(text: comment_parameters[:text], author_id: current_user.id, post_id: @post.id)
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = @post.comments.create(text: comment_parameters[:text], author_id: current_user.id, post_id: @post.id)
 
-        if @comment.save
-            respond_to do |format|
-                firmat.html { redirect_to user_post_path(current_user, @post), notice: 'Comment created successfully' }
+    if @comment.save
+      respond_to do |format|
+        firmat.html { redirect_to user_post_path(current_user, @post), notice: 'Comment created successfully' }
 
-                format.json { render json: @comment, status: :created }
-            end
-        else
-            respond_to do |format|
-                format.json { render json: @comment.errors, status: :unprocessable_entity }
-            end
-        end
+        format.json { render json: @comment, status: :created }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    private
+  private
 
-    def comment_parameters
-        params.require(:comment).permit(:text)
-    end
+  def comment_parameters
+    params.require(:comment).permit(:text)
+  end
 end
